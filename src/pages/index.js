c5a92e9031a647017b50ea6ceff958414564ffac
Zach Shirah <zachshirah@MacBook-Pro.attlocal.net>
2020-09-06 14:06:00 -0400

    changed index page to have an index component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,25 +4,27 @@ import "../styles.css"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Headshot from "../components/Headshot"
-import ProfessionalSummary from "../components/ProfessionalSummary"
+import IndexComponent from "../components/IndexComponent"
+// import Headshot from "../components/Headshot"
+// import ProfessionalSummary from "../components/ProfessionalSummary"
 
-import styled from "styled-components"
+// import styled from "styled-components"
 
-const SummaryHeadshotContainer = styled.div`
-  padding-top: calc(50px + 10vh);
-  display: flex;
-  flex-direction: row;
-`
+// const SummaryHeadshotContainer = styled.div`
+//   padding-top: calc(50px + 10vh);
+//   display: flex;
+//   flex-direction: row;
+// `
 
 const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
-        <SummaryHeadshotContainer>
+        <IndexComponent />
+        {/* <SummaryHeadshotContainer>
           <Headshot />
           <ProfessionalSummary />
-        </SummaryHeadshotContainer>
+        </SummaryHeadshotContainer> */}
     </Layout>
   )
 }
